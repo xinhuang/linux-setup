5d1b4807a062ba4e02e6ede51c50c8b9232f65ae
xinhuang <xinhuang.abc@gmail.com>
2016-03-25 13:36:31 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+add-apt-repository -y ppa:saiarcot895/myppa     # apt-fast
+add-apt-repository -y ppa:jfi/myppa             # psensor
+add-apt-repository -y ppa:webupd8team/atom      # atom
+add-apt-repository -y ppa:webupd8team/java      # java
+
+apt-get update
+
+apt-get -y install apt-fast
+
+apt-fast -y install zsh git vim python g++ ruby gem cmake gdb gcc
+apt-fast -y install psensor atom oracle-java7-installer oracle-java8-installer
+
+# rvm-prompt for oh-my-zsh
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# vim configuration
+# TODO: get .vimrc
+vim +PluginInstall +qall
