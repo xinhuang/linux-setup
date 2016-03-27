660acc0fb29f57d67768adca621e66f7fe4ebfee
xinhuang <xinhuang.abc@gmail.com>
2016-03-27 13:50:25 +0800

    add libreoffice

--- install.sh
+++ install.sh
@@ -4,26 +4,35 @@ add-apt-repository -y ppa:saiarcot895/myppa     # apt-fast
 add-apt-repository -y ppa:jfi/myppa             # psensor
 add-apt-repository -y ppa:webupd8team/atom      # atom
 add-apt-repository -y ppa:webupd8team/java      # java
-add-apt-repository ppa:mpstark/elementary-tweaks-daily     # elementary-tweaks
-sudo add-apt-repository ppa:cybre/elementaryplus    # more elementary icons
+add-apt-repository -y ppa:mpstark/elementary-tweaks-daily     # elementary-tweaks
+add-apt-repository -y ppa:cybre/elementaryplus  # more elementary icons
+add-apt-repository -y ppa:libreoffice/ppa       # libre office
 
 apt-get update
 
 apt-get -y install apt-fast
 
 apt-fast -y install zsh git vim python g++ ruby gem cmake gdb gcc
-apt-fast -y install psensor atom oracle-java7-installer oracle-java8-installer
+apt-fast -y install psensor atom 
 
 # rvm-prompt for oh-my-zsh
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
 # vim configuration
 wget -O ~/.vimrc 'https://gist.githubusercontent.com/xinhuang/4336621/raw/935ca0812db4df77819a7eb27884e57b201207ba/vimrc'
 vim +PluginInstall +qall
 
-apt-get install ubuntu-restricted-extras
-apt-fast install elementary-wallpapers-extra
-apt-get install elementaryplus
+apt-fast install -y ubuntu-restricted-extras
+apt-fast install -y elementary-wallpapers-extra
+apt-fast install -y elementaryplus
+
+# these are the slow ones
+apt-fast install -y libreoffice
+apt-fast install -y oracle-java7-installer oracle-java8-installer
+
+# at last we install oh-my-zsh and switch to zsh shell
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+echo "Please modify /etc/passwd file to change default shell to zsh."
+
