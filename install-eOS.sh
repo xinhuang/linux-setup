af92f79133134374297b48dab14a26af5f0099cc
xinhuang <xinhuang.abc@gmail.com>
2016-08-27 20:36:38 +0800

    install.sh for Ubuntu; rename previous install.sh for elementaryOS

--- install-eOS.sh
+++ install-eOS.sh
@@ -1,26 +1,19 @@
 #!/bin/sh
 
 add-apt-repository -y ppa:saiarcot895/myppa     # apt-fast
-# add-apt-repository -y ppa:jfi/myppa             # psensor
+add-apt-repository -y ppa:jfi/myppa             # psensor
 add-apt-repository -y ppa:webupd8team/atom      # atom
 add-apt-repository -y ppa:webupd8team/java      # java
+add-apt-repository -y ppa:mpstark/elementary-tweaks-daily     # elementary-tweaks
+add-apt-repository -y ppa:cybre/elementaryplus  # more elementary icons
 add-apt-repository -y ppa:libreoffice/ppa       # libre office
 
 apt-get update
 
 apt-get -y install apt-fast
 
-apt-fast -y install zsh git vim g++ ruby gem cmake gdb gcc
+apt-fast -y install zsh git vim python g++ ruby gem cmake gdb gcc
 apt-fast -y install psensor atom 
-
-# development libraries
-# threading building blocks
-apt-fast -y install libtbb2 libtbb-dev
-
-# python & python packages
-apt-fast -y install python python-pip
-pip install pip --upgrade
-pip install ipython numpy
 
 # rvm-prompt for oh-my-zsh
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
@@ -31,8 +24,8 @@ wget -O ~/.vimrc 'https://gist.githubusercontent.com/xinhuang/4336621/raw/935ca0
 vim +PluginInstall +qall
 
 apt-fast install -y ubuntu-restricted-extras
-# apt-fast install -y elementary-wallpapers-extra
-# apt-fast install -y elementaryplus
+apt-fast install -y elementary-wallpapers-extra
+apt-fast install -y elementaryplus
 
 # these are the slow ones
 apt-fast install -y libreoffice
