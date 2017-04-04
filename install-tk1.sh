4d8092fc352c5a82fd16075c989f3a5afd28d3bb
xinhuang <xinhuang.abc@gmail.com>
2017-04-04 02:06:21 -0700

    [tk1] merge from install.sh

--- install-tk1.sh
+++ install-tk1.sh
@@ -17,6 +17,8 @@ sudo apt-get -y install apt-fast
 
 sudo apt-fast -y install zsh git vim g++ ruby gem cmake gdb gcc
 sudo apt-fast -y install lm-sensors
+sudo apt-fast -y install apt-file
+apt-file update
 
 # development libraries
 # threading building blocks
@@ -24,8 +26,8 @@ sudo apt-fast -y install libtbb2 libtbb-dev
 
 # python & python packages
 sudo apt-fast -y install python python-dev python-pip
-sudo pip install pip --upgrade
-sudo pip install ipython numpy 
+sudo -H pip install pip --upgrade
+sudo -H pip install ipython numpy jupyter
 
 # rvm-prompt for oh-my-zsh
 sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
@@ -40,10 +42,9 @@ vim +PluginInstall +qall
 
 # sudo apt-fast install -y oracle-java7-installer oracle-java8-installer
 
+sudo apt-fast -y install autojump
 # at last we install oh-my-zsh and switch to zsh shell
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 echo "Please modify /etc/passwd file to change default shell to zsh."
-
-sudo apt-fast -y install autojump
 
