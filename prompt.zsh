fdfb775668703efd3f9f52d1d537cc5465ba8e88
xinhuang <xinhuang.abc@gmail.com>
2017-04-06 08:44:19 +0000

    [prompt] fix temperature is not re-calculated every time

--- prompt.zsh
+++ prompt.zsh
@@ -1,5 +1,7 @@
 function show_temp(){                                                             
-    sensors | grep -oP 'temp1:\s+\K([-\+][\d.]+°C)' -m 1                          
+    sensors | grep -oP '[Tt]emp.?:\s+\K([-\+][\d.]+°C)' -m 1                          
 }
 
-RPROMPT="$RPROMPT $(show_temp)"
+local temp_special='$(show_temp)'
+
+RPROMPT="$RPROMPT ${temp_special}"
